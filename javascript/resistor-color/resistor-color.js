3dafe8dfbdf42e51ee71c3089eb36dba0ddce0d5
Shi Han NG <shihanng@gmail.com>
2019-07-12 20:24:43 +0900

    javascript: Solve Resistor Color

---
+++ javascript/resistor-color/resistor-color.js
@@ -0,0 +1,21 @@
+//
+// This is only a SKELETON file for the 'Resistor Color' exercise. It's been provided as a
+// convenience to get you started writing code faster.
+//
+
+export const colorCode = c => {
+  return COLORS.indexOf(c);
+};
+
+export const COLORS = [
+  "black",
+  "brown",
+  "red",
+  "orange",
+  "yellow",
+  "green",
+  "blue",
+  "violet",
+  "grey",
+  "white"
+];
