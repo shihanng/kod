99988cca09773071ec326643866216c114838d23
Shi Han NG <shihanng@gmail.com>
2019-07-16 21:15:33 +0900

    ruby: Use enumerator

--- ruby/resistor-color-duo/resistor_color_duo.rb
+++ ruby/resistor-color-duo/resistor_color_duo.rb
@@ -1,24 +1,22 @@
-COLORS = %w[
-  black
-  brown
-  red
-  orange
-  yellow
-  green
-  blue
-  violet
-  grey
-  white
-].freeze
+# frozen_string_literal: true
+
+COLORS = {
+  black: 0,
+  brown: 1,
+  red: 2,
+  orange: 3,
+  yellow: 4,
+  green: 5,
+  blue: 6,
+  violet: 7,
+  grey: 8,
+  white: 9
+}.freeze
 
 module ResistorColorDuo
   def self.value(colors)
-    val = 0
-
-    colors.reverse.each_with_index do |color, i|
-      val += COLORS.index(color) * 10**i
+    colors.reverse.map.each_with_index.sum do |color, i|
+      COLORS[color.to_sym] * 10**i
     end
-
-    val
   end
 end
