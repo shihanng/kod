96a6a840f68cc46072ca013d43dd1abfaa5d1ca0
Shi Han NG <shihanng@gmail.com>
2019-07-17 21:29:09 +0900

    ruby: Solve with reducer

--- ruby/resistor-color-duo/resistor_color_duo.rb
+++ ruby/resistor-color-duo/resistor_color_duo.rb
@@ -1,22 +1,23 @@
 # frozen_string_literal: true
 
 COLORS = {
-  black: 0,
-  brown: 1,
-  red: 2,
-  orange: 3,
-  yellow: 4,
-  green: 5,
-  blue: 6,
-  violet: 7,
-  grey: 8,
-  white: 9
+  black: '0',
+  brown: '1',
+  red: '2',
+  orange: '3',
+  yellow: '4',
+  green: '5',
+  blue: '6',
+  violet: '7',
+  grey: '8',
+  white: '9'
 }.freeze
 
 module ResistorColorDuo
   def self.value(colors)
-    colors.reverse.map.each_with_index.sum do |color, i|
-      COLORS[color.to_sym] * 10**i
+    res = colors.map.reduce('') do |r, color|
+      r + COLORS[color.to_sym]
     end
+    res.to_i
   end
 end
