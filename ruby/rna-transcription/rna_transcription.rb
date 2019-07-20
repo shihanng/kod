663ea6aa64921838f5448cd7c9595147cee91940
Shi Han NG <shihanng@gmail.com>
2019-07-20 21:56:36 +0900

    ruby: Use DNA2RNA as constant

--- ruby/rna-transcription/rna_transcription.rb
+++ ruby/rna-transcription/rna_transcription.rb
@@ -1,5 +1,7 @@
 module Complement
+  @DNA2RNA = { 'G' => 'C', 'C' => 'G', 'T' => 'A', 'A' => 'U' }
+
   def self.of_dna(dna)
-    dna.gsub(/[GCTA]/, 'G' => 'C', 'C' => 'G', 'T' => 'A', 'A' => 'U')
+    dna.gsub(/[GCTA]/, @DNA2RNA)
   end
 end
