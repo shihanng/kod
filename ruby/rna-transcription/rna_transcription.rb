822aa252140625de66193a7abc76e303af711e3a
Shi Han NG <shihanng@gmail.com>
2019-07-22 08:27:14 +0900

    ruby: Put constant into Class

--- ruby/rna-transcription/rna_transcription.rb
+++ ruby/rna-transcription/rna_transcription.rb
@@ -1,6 +1,7 @@
-DNA2RNA = { 'G' => 'C', 'C' => 'G', 'T' => 'A', 'A' => 'U' }.freeze
 
 module Complement
+  DNA2RNA = { 'G' => 'C', 'C' => 'G', 'T' => 'A', 'A' => 'U' }.freeze
+
   def self.of_dna(dna)
     dna.gsub(/[GCTA]/, DNA2RNA)
   end
