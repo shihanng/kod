9c4626e451c4850b205eb445b89f32df1fd094d9
Shi Han NG <shihanng@gmail.com>
2019-08-19 06:15:24 +0900

    javascript: Use GIGASECOND_IN_MS

--- javascript/gigasecond/gigasecond.js
+++ javascript/gigasecond/gigasecond.js
@@ -1,6 +1,3 @@
-//
-// This is only a SKELETON file for the 'Gigasecond' exercise. It's been provided as a
-// convenience to get you started writing code faster.
-//
+const GIGASECOND_IN_MS = 1e12;
 
-export const gigasecond = date => new Date(date.getTime() + 1e12);
+export const gigasecond = date => new Date(date.getTime() + GIGASECOND_IN_MS);
