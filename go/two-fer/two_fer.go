0fbaf39b57609903133a7e35d653e8788b1518dd
Shi Han NG <shihanng@gmail.com>
2020-09-24 20:38:38 +0900

    go: Improve two-fer

--- go/two-fer/two_fer.go
+++ go/two-fer/two_fer.go
@@ -1,16 +1,12 @@
-// This is a "stub" file.  It's a little start on your solution.
-// It's not a complete solution though; you have to write some code.
-
-// Package twofer should have a package comment that summarizes what it's about.
-// https://golang.org/doc/effective_go.html#commentary
+// Package twofer is short for two for one. One for you and one for me.
 package twofer
 
 import "fmt"
 
-// ShareWith should have a comment documenting it.
+// ShareWith uses "you" as name if name is empty.
 func ShareWith(name string) string {
 	if name == "" {
-		return "One for you, one for me."
+		name = "you"
 	}
 
 	return fmt.Sprintf("One for %s, one for me.", name)
