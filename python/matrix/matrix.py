68f48945fe24c17f66851787cdae15e2933f8fe8
Shi Han NG <shihanng@gmail.com>
2020-09-26 15:45:03 +0900

    python: Reduce space usage in Matrix

--- python/matrix/matrix.py
+++ python/matrix/matrix.py
@@ -2,10 +2,13 @@ class Matrix:
     def __init__(self, matrix_string):
         rows = matrix_string.splitlines()
         self.matrix = [[int(elm) for elm in r.split(" ")] for r in rows]
-        self.transpose_matrix = list(map(list, zip(*self.matrix)))
 
     def row(self, index):
         return self.matrix[index - 1]
 
     def column(self, index):
-        return self.transpose_matrix[index - 1]
+        return getColumn(index - 1, self.matrix)
+
+
+def getColumn(index, matrix):
+    return [r[index] for r in matrix]
