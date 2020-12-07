75b33e8312853d8446f526231e7c1a2a1eb43dca
Shi Han NG <shihanng@gmail.com>
2020-12-07 20:54:31 +0900

    Add fastFib

--- main.go
+++ main.go
@@ -6,7 +6,9 @@ func main() {
 	fmt.Println(fib(6))
 	fmt.Println(fib(7))
 	fmt.Println(fib(9))
-	fmt.Println(fib(50))
+
+	fb := make(fastFib)
+	fmt.Println(fb.fib(50))
 }
 
 func fib(n int) int {
@@ -14,4 +16,20 @@ func fib(n int) int {
 		return 1
 	}
 	return fib(n-1) + fib(n-2)
+}
+
+type fastFib map[int]int
+
+func (f fastFib) fib(n int) int {
+	res, ok := f[n]
+	if ok {
+		return res
+	}
+
+	if n <= 2 {
+		return 1
+	}
+
+	f[n] = f.fib(n-1) + f.fib(n-2)
+	return f[n]
 }
