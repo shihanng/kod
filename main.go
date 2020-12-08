393f61d19b387813962a3e0252541868abdbf773
Shi Han NG <shihanng@gmail.com>
2020-12-08 20:05:21 +0900

    Add solution to gridTraveler

--- main.go
+++ main.go
@@ -9,6 +9,13 @@ func main() {
 
 	fb := make(fastFib)
 	fmt.Println(fb.fib(50))
+
+	fmt.Println(gridTraveler(1, 1))
+	fmt.Println(gridTraveler(2, 3))
+	fmt.Println(gridTraveler(3, 2))
+
+	ft := make(memoGridTraveler)
+	fmt.Println(ft.travel(18, 18))
 }
 
 func fib(n int) int {
@@ -32,4 +39,38 @@ func (f fastFib) fib(n int) int {
 
 	f[n] = f.fib(n-1) + f.fib(n-2)
 	return f[n]
+}
+
+func gridTraveler(m, n int) int {
+	if m == 1 && n == 1 {
+		return 1
+	}
+
+	if m == 0 || n == 0 {
+		return 0
+	}
+
+	return gridTraveler(m-1, n) + gridTraveler(m, n-1)
+}
+
+type memoGridTraveler map[string]int
+
+func (t memoGridTraveler) travel(m, n int) int {
+	key := fmt.Sprintf("%d,%d", m, n)
+
+	val, ok := t[key]
+	if ok {
+		return val
+	}
+
+	if m == 1 && n == 1 {
+		return 1
+	}
+
+	if m == 0 || n == 0 {
+		return 0
+	}
+
+	t[key] = t.travel(m-1, n) + t.travel(m, n-1)
+	return t[key]
 }
