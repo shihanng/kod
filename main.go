b26495809f7c37a2241f301ab7ca58e424c4248d
Shi Han NG <shihanng@gmail.com>
2020-12-10 21:46:12 +0900

    Solve howSum

--- main.go
+++ main.go
@@ -123,3 +123,53 @@ func (c fastCanSum) canSum(targetSum int, numbers []int) bool {
 
 	return false
 }
+
+func howSum(targetSum int, numbers []int) []int {
+	if targetSum == 0 {
+		return []int{}
+	}
+
+	if targetSum < 0 {
+		return nil
+	}
+
+	for _, n := range numbers {
+		remainder := targetSum - n
+
+		res := howSum(remainder, numbers)
+		if res != nil {
+			return append(res, n)
+		}
+	}
+
+	return nil
+}
+
+type fastHowSum map[int][]int
+
+func (s fastHowSum) howSum(targetSum int, numbers []int) []int {
+	if targetSum == 0 {
+		return []int{}
+	}
+
+	if targetSum < 0 {
+		return nil
+	}
+
+	if res, ok := s[targetSum]; ok {
+		return res
+	}
+
+	for _, n := range numbers {
+		remainder := targetSum - n
+
+		s[targetSum] = s.howSum(remainder, numbers)
+		if s[targetSum] != nil {
+			s[targetSum] = append(s[targetSum], n)
+			return s[targetSum]
+		}
+	}
+
+	s[targetSum] = nil
+	return s[targetSum]
+}
--- main_test.go
+++ main_test.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"reflect"
 	"testing"
 )
 
@@ -83,6 +84,83 @@ func Test_fastCanSum_canSum(t *testing.T) {
 		t.Run(fmt.Sprintf("case-%d", i), func(t *testing.T) {
 			if got := tt.c.canSum(tt.args.targetSum, tt.args.numbers); got != tt.want {
 				t.Errorf("fastCanSum.canSum() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func Test_howSum(t *testing.T) {
+	type args struct {
+		targetSum int
+		numbers   []int
+	}
+	tests := []struct {
+		args args
+		want []int
+	}{
+		{
+			args: args{
+				targetSum: 7,
+				numbers:   []int{2, 3},
+			},
+			want: []int{3, 2, 2},
+		},
+		{
+			args: args{
+				targetSum: 7,
+				numbers:   []int{2, 4},
+			},
+			want: nil,
+		},
+	}
+	for i, tt := range tests {
+		t.Run(fmt.Sprintf("case-%d", i), func(t *testing.T) {
+			if got := howSum(tt.args.targetSum, tt.args.numbers); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("howSum() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func Test_fastHowSum_howSum(t *testing.T) {
+	type args struct {
+		targetSum int
+		numbers   []int
+	}
+	tests := []struct {
+		s    fastHowSum
+		args args
+		want []int
+	}{
+		{
+			s: make(fastHowSum),
+			args: args{
+				targetSum: 7,
+				numbers:   []int{2, 4},
+			},
+			want: nil,
+		},
+		{
+			s: make(fastHowSum),
+			args: args{
+				targetSum: 8,
+				numbers:   []int{2, 3, 5},
+			},
+			want: []int{2, 2, 2, 2},
+		},
+		{
+			s: make(fastHowSum),
+			args: args{
+				targetSum: 300,
+				numbers:   []int{7, 14},
+			},
+			want: nil,
+		},
+	}
+	for i, tt := range tests {
+		t.Run(fmt.Sprintf("case-%d", i), func(t *testing.T) {
+			if got := tt.s.howSum(tt.args.targetSum, tt.args.numbers); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("fastHowSum.howSum() = %v, want %v", got, tt.want)
 			}
 		})
 	}
