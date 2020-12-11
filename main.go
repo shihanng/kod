0c2b73c5560a3ff6d2e88da34535ab8fef9daca5
Shi Han NG <shihanng@gmail.com>
2020-12-11 21:32:03 +0900

    Solve bestSum

--- main.go
+++ main.go
@@ -173,3 +173,39 @@ func (s fastHowSum) howSum(targetSum int, numbers []int) []int {
 	s[targetSum] = nil
 	return s[targetSum]
 }
+
+type bestSum map[int][]int
+
+func (s bestSum) bestSum(targetSum int, numbers []int) []int {
+	if targetSum == 0 {
+		return []int{}
+	}
+
+	if targetSum < 0 {
+		return nil
+	}
+
+	if res, ok := s[targetSum]; ok {
+		return res
+	}
+
+	var shortest []int
+
+	for _, n := range numbers {
+		remainder := targetSum - n
+
+		res := s.bestSum(remainder, numbers)
+		if res == nil {
+			continue
+		}
+
+		res = append(res, n)
+
+		if len(shortest) == 0 || len(res) < len(shortest) {
+			shortest = res
+		}
+	}
+
+	s[targetSum] = shortest
+	return shortest
+}
