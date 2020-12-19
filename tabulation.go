11c7d31ba9d156ee2e3800bbfb74bb1491c508fc
Shi Han NG <shihanng@gmail.com>
2020-12-19 10:31:06 +0900

    Add howSumTabulation

--- tabulation.go
+++ tabulation.go
@@ -73,3 +73,22 @@ func howSumTabulation(targetSum int, numbers []int) []int {
 
 	return targets[targetSum]
 }
+
+func bestSumTabulation(targetSum int, numbers []int) []int {
+	targets := make([][]int, targetSum+1)
+
+	targets[0] = []int{}
+
+	for i := range targets {
+		if targets[i] != nil {
+			for _, n := range numbers {
+				newSum := append(targets[i], n)
+				if i+n <= targetSum && (targets[i+n] == nil || len(newSum) < len(targets[i+n])) {
+					targets[i+n] = newSum
+				}
+			}
+		}
+	}
+
+	return targets[targetSum]
+}
--- tabulation_test.go
+++ tabulation_test.go
@@ -174,11 +174,66 @@ func Test_howSumTabulation(t *testing.T) {
 			},
 			want: nil,
 		},
+		{
+			args: args{
+				targetSum: 100,
+				numbers:   []int{1, 2, 5, 25},
+			},
+			want: []int{25, 25, 25, 25},
+		},
 	}
 	for i, tt := range tests {
 		t.Run(fmt.Sprintf("case-%d", i), func(t *testing.T) {
 			if got := howSumTabulation(tt.args.targetSum, tt.args.numbers); !reflect.DeepEqual(got, tt.want) {
 				t.Errorf("howSumTabulation() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func Test_bestSumTabulation(t *testing.T) {
+	type args struct {
+		targetSum int
+		numbers   []int
+	}
+	tests := []struct {
+		name string
+		args args
+		want []int
+	}{
+		{
+			args: args{
+				targetSum: 7,
+				numbers:   []int{5, 3, 4, 7},
+			},
+			want: []int{7},
+		},
+		{
+			args: args{
+				targetSum: 8,
+				numbers:   []int{2, 3, 5},
+			},
+			want: []int{3, 5},
+		},
+		{
+			args: args{
+				targetSum: 8,
+				numbers:   []int{1, 4, 5},
+			},
+			want: []int{4, 4},
+		},
+		{
+			args: args{
+				targetSum: 100,
+				numbers:   []int{1, 2, 5, 25},
+			},
+			want: []int{25, 25, 25, 25},
+		},
+	}
+	for i, tt := range tests {
+		t.Run(fmt.Sprintf("case-%d", i), func(t *testing.T) {
+			if got := bestSumTabulation(tt.args.targetSum, tt.args.numbers); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("bestSumTabulation() = %v, want %v", got, tt.want)
 			}
 		})
 	}
